38b935f302e4112f4031d0db3bc29c8830fd49ed
Phiwokwakhe Khathwane <phiwokwakhe299@gmail.com>
2024-02-26 15:13:02 +0200

    Initial Commit.

---
+++ HotelManangementSystemLibrary/Model/Interfaces/IPerson.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HotelManangementSystemLibrary
+{
+    public interface IPerson
+    {
+        string Name { get;}
+        string Surname { get;}
+        DateTime DOB { get;}
+    }//IPerson
+}//namespace
