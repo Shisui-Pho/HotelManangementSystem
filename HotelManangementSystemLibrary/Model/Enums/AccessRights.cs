b90771fffd8e3ccd18f9acca0d7cca0d14f7f016
Phiwokwakhe Khathwane <phiwokwakhe299@gmail.com>
2024-02-27 16:39:34 +0200

    Added enums for the AccessRights for the administrator and for the types of users

---
+++ HotelManangementSystemLibrary/Model/Enums/AccessRights.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HotelManangementSystemLibrary
+{
+    public enum AccessRights
+    {
+        Default = 0,
+        Universal,
+        Restricted
+    }//
+}//class
---
+++ HotelManangementSystemLibrary/Model/Enums/TypeOfUser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HotelManangementSystemLibrary
+{
+    public enum TypeOfUser
+    {
+        Default = 0,
+        Admin,
+        Mananger,
+        Employee,
+        Guest
+    }//TypeOfUser
+}//namespace
