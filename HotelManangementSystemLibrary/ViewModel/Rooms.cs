13914748da7c51736e2c438b6afb78a000ebc4ce
Phiwokwakhe Khathwane <phiwokwakhe299@gmail.com>
2024-02-27 16:43:47 +0200

    Added containers of the users and rooms

---
+++ HotelManangementSystemLibrary/ViewModel/Rooms.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HotelManangementSystemLibrary
+{
+    public class Rooms : IRooms
+    {
+        private List<IRoom> _rooms;
+        public int Count => _rooms.Count;
+
+        public Rooms()
+        {
+            _rooms = new List<IRoom>();
+        }//ctor 01
+
+        public Rooms(List<IRoom> rooms)
+        {
+            _rooms = rooms;
+        }//ctor 02
+
+        public Rooms(IRoom[] rooms)
+        {
+            _rooms = new List<IRoom>();
+            _rooms.AddRange(rooms);
+        }//ctor 03
+
+        public void Add(IRoom room)
+           => _rooms.Add(room);
+
+        public void Remove(IRoom room)
+            => _rooms.Remove(room);
+
+        public void Update(IRoom old, IRoom _new)
+        {
+            int i = _rooms.FindIndex(rm => rm.RoomNumber == old.RoomNumber);
+            if (i < 0)
+                throw new ArgumentException($"The room number \"{old.RoomNumber}\" was not found");
+            _rooms[i] = _new;
+        }//Update
+        public IEnumerator<IRoom> GetEnumerator()
+        {
+            foreach (IRoom room in _rooms)
+            {
+                yield return room;
+            }
+        }//GetEnumerator
+    }//Rooms
+}//namespace
---
+++ HotelManangementSystemLibrary/ViewModel/Users.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HotelManangementSystemLibrary
+{
+    public class Users : IUsers
+    {
+        private readonly List<IUser> _users;
+        public int Count => _users.Count;
+
+        public Users()
+        {
+            _users = new List<IUser>();
+        }//ctor 1
+        public Users(IUser[] users)
+        {
+            _users = new List<IUser>();
+            _users.AddRange(users);
+        }//ctor 2
+        public Users(List<IUser> users)
+        {
+            _users = users;
+        }//ctor 2
+        public void Add(IUser user)
+            => _users.Add(user);
+
+        public bool AlreadyExist(IUser user)
+        {
+            IUser _user = _users.FirstOrDefault(u => u.UserID == user.UserID);
+            if (_user is null)
+                return false;
+            return true;
+        }//AlreadyExist
+
+        public void Remove(IUser user)
+            => _users.Remove(user);
+
+        public void Update(IUser old, IUser _new)
+        {
+            int i = _users.FindIndex(us => us.UserID == old.UserID);
+            if (i < 0)
+                throw new ArgumentException("User not found");
+            _users[i] = _new;
+        }//Update
+        public IEnumerator<IUser> GetEnumerator()
+        {
+            foreach (IUser user in _users)
+                yield return user;
+        }//GetEnumerator
+    }//class
+}//namespace
