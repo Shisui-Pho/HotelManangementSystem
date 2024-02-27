28d1483aaf4b31a9ac668c2ae1957213a354fa84
Phiwokwakhe Khathwane <phiwokwakhe299@gmail.com>
2024-02-27 16:41:57 +0200

    Created models for the rooms and the room bookings

---
+++ HotelManangementSystemLibrary/Model/Room.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HotelManangementSystemLibrary
+{
+    public abstract class Room : IRoom
+    {
+        public string RoomNumber { get; private set; }
+
+        public bool HasTV { get; protected set; } = false;
+
+        public bool IsSingleRoom { get; protected set; }
+
+        public decimal Price { get; protected set; }
+        public Room(string _roomNumber)
+        {
+            RoomNumber = _roomNumber;
+        }//ctor
+
+        public void ChangeRoomNumber(string _newRoomNumber)
+        {
+            RoomNumber = _newRoomNumber;
+        }//ChangeRoomNumber
+
+        public void AddTV()
+        {
+            HasTV = true;
+        }//AddTV
+        public void RemoveTV()
+        {
+            HasTV = false;
+        }//RemoveTV
+    }//class
+    public class SingleRoom : Room , ISingleRoom
+    {
+        public SingleRoom(string _roomNumber, bool hasTv = false): base(_roomNumber)
+        {
+            HasTV = hasTv;
+            IsSingleRoom = true;
+        }//ctor
+    }//Singleroom
+
+    public class DoubleRoom : Room , IDoubleRoom
+    {
+        public DoubleRoom(string _roomNumber, bool hasTv = false) : base(_roomNumber)
+        {
+            HasTV = hasTv;
+            IsSingleRoom = true;
+        }//ctor
+    }//DoubleRoom
+}//namespace
---
+++ HotelManangementSystemLibrary/Model/RoomBooking.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HotelManangementSystemLibrary
+{
+    public class RoomBooking : IRoomBooking
+    {
+        //Data member
+        private decimal _amount;
+
+        //Properties
+        public IGuest Guest { get; private set; }
+
+        public IRoom Room { get; private set; }
+
+        public DateTime DateBookedFor { get; private set; }
+
+        public int NumberOfDaysToStay { get; private set; }
+
+        public decimal AmoutToPay => _amount;
+
+        public RoomBooking(IGuest guest, IRoom room, DateTime date, int numberOfDays = 1)
+        {
+            Guest = guest;
+            Room = room;
+            DateBookedFor = date;
+            NumberOfDaysToStay = numberOfDays;
+            _amount = Room.Price * numberOfDays;
+        }//RoomBooking
+    }//class
+}//namespace
