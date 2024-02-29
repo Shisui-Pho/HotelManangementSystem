7587e4d0683dfc8f8d61d9c6778d96728afe730e
Phiwokwakhe Khathwane <phiwokwakhe299@gmail.com>
2024-02-29 09:17:07 +0200

    Added a database class for reading data from a textfile/csv

---
+++ HotelManangementSystemLibrary/DatabaseService/TextFileDatabase.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HotelManangementSystemLibrary.DatabaseService
+{
+    public class TextFileDatabase : IDatabaseService
+    {
+        //DateMembers
+        private IRoomBookings _bookings = null;
+        private IRooms _rooms = null;
+        private IUsers _users = null;
+
+        public IRoomBookings Bookings 
+        {
+            get
+            {
+                if (_bookings is null)
+                    return LoadBookings();
+                return _bookings;
+            }//get
+        }//Bookings
+        public IRooms Rooms
+        {
+            get
+            {
+                if (_rooms is null)
+                    return LoadRooms();
+                return _rooms;
+            }//get
+        }//Bookings
+        public IUsers Users
+        {
+            get
+            {
+                if (_users is null)
+                    return LoadUsers();
+                return _users;
+            }//get
+        }//Bookings
+        public TextFileDatabase()
+        {
+        }//ctor 01
+        public IRoomBookings LoadBookings()
+        {
+            return default;
+        }//LoadBookings
+
+        public IRooms LoadRooms()
+        {
+            return default;
+        }//LoadRooms
+
+        public IUsers LoadUsers()
+        {
+            return default;
+        }//LoadUsers
+
+        public void SaveBookings()
+        {
+
+        }//SaveBookings
+
+        public void SaveRooms()
+        {
+        }//SaveRooms
+
+        public void SaveUsers()
+        {
+        }//SaveUsers
+    }//class
+}//namespace
