02a20f4ed6a218d84dcb3ee3debd53bc27ec68c7
Phiwokwakhe Khathwane <phiwokwakhe299@gmail.com>
2024-02-29 09:16:18 +0200

    Addded a database Service Interface

---
+++ HotelManangementSystemLibrary/DatabaseService/Interfacae/IDatabaseService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HotelManangementSystemLibrary.DatabaseService
+{
+    public interface IDatabaseService
+    {
+        IRoomBookings Bookings { get; }
+        IRooms Rooms { get; }
+        IUsers Users { get; }
+        void SaveBookings();
+        void SaveRooms();
+        void SaveUsers();
+        IRoomBookings LoadBookings();
+        IRooms LoadRooms();
+        IUsers LoadUsers();
+    }//interface
+}//namespace
