af0b6ef2bc9c04e3328d9a01f8d7b4ecabe5587a
Phiwokwakhe Khathwane <phiwokwakhe299@gmail.com>
2024-02-29 11:01:58 +0200

    Added guests container class

---
+++ HotelManangementSystemLibrary/ViewModel/Guests.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+namespace HotelManangementSystemLibrary
+{
+    public class Guests : IGuests
+    {
+        private List<IGuest> _guests;
+        public int Count => _guests.Count;
+        public Guests()
+        {
+            _guests = new List<IGuest>();
+        }
+
+        public void Add(IGuest guest)
+            => _guests.Add(guest);
+
+        public void Remove(IGuest guest)
+            => _guests.Remove(guest);
+
+        public void Update(IGuest old, IGuest _new)
+        {
+            int i = _guests.IndexOf(old);
+            if (i < 0)
+                throw new KeyNotFoundException();
+            _guests[i] = _new;
+        }//Update
+        public IEnumerator<IGuest> GetEnumerator()
+        {
+            foreach (IGuest guest in _guests)
+                yield return guest;
+        }//GetEnumerator
+
+        public IGuest FindGuest(string guestId)
+        {
+            return _guests.FirstOrDefault(g => g.UserID == guestId);
+        }//FindGuest
+    }//class
+    public interface IGuests : ICollectionHotel<IGuest>
+    {
+        IGuest FindGuest(string guestId);
+    }//interface
+}//namespace
---
+++ HotelManangementSystemLibrary/ViewModel/Interfaces/IGuests.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HotelManangementSystemLibrary
+{
+    public interface IGuests : ICollectionHotel<IGuest>
+    {
+        IGuest FindGuest(string guestId);
+    }//interface
+}//namespace
