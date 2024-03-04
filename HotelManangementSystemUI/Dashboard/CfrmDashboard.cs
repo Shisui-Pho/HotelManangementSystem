f8a93901a6fd6e6bad8c6333474869a9bdb0b85b
Phiwokwakhe Khathwane <phiwokwakhe299@gmail.com>
2024-03-04 10:02:56 +0200

    Added a dashboard /main form.

---
+++ HotelManangementSystemUI/Dashboard/CfrmDashboard.cs
@@ -0,0 +1,23 @@
+﻿using HotelManangementSystemLibrary.DatabaseService;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HotelManangementSystemUI.Dashboard
+{
+    public partial class CfrmDashboard : Form
+    {
+        private readonly IDatabaseService database;
+        public CfrmDashboard(IDatabaseService database)
+        {
+            InitializeComponent();
+            this.database = database;
+        }//ctor 01
+    }//class
+}//namespace
