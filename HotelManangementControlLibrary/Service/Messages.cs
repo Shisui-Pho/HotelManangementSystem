13945ab19c3cc904a7635fd438116787f5b80186
Phiwokwakhe Khathwane <phiwokwakhe299@gmail.com>
2024-03-05 16:01:40 +0200

    Added helper method for standar messages

---
+++ HotelManangementControlLibrary/Service/Messages.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HotelManangementControlLibrary.Service
+{
+    public static class Messages
+    {
+        public static void ShowErrorMessage(string sMsg, string title)
+        {
+            MessageBox.Show(sMsg, title, MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }        
+        public static void ShowInformationMessage(string sMsg, string title)
+        {
+            MessageBox.Show(sMsg, title, MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+        public static DialogResult AskYesOrNot(string sMsg, string title)
+        {
+            return MessageBox.Show(sMsg, title, MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+        }
+    }
+}
