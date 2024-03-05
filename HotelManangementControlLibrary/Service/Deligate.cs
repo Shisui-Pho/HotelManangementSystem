4b02393efab38f845270922aa3b979ccbe5a1fdc
Phiwokwakhe Khathwane <phiwokwakhe299@gmail.com>
2024-03-05 16:04:32 +0200

    Added a method defination for booking and cancelling.

--- HotelManangementControlLibrary/Service/Deligate.cs
+++ HotelManangementControlLibrary/Service/Deligate.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HotelManangementSystemLibrary;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,4 +8,6 @@ using System.Threading.Tasks;
 namespace HotelManangementControlLibrary
 {
     public delegate void ChangeControl();
+    public delegate void BookRoom(IRoom room);//The user will be the current user
+    public delegate void CancelBooking(IRoom room);//The user will be the current user
 }//namespace
