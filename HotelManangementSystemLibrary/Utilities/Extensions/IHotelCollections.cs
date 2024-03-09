fdd8551c0f29339d4c6fae7928d2cc615532b3e1
Phiwokwakhe Khathwane <phiwokwakhe299@gmail.com>
2024-03-09 20:17:33 +0200

    Added a general extension for the ICollectionHotel interface for saving data using the IHotelModel which contain ToCSVFormat method.

---
+++ HotelManangementSystemLibrary/Utilities/Extensions/IHotelCollections.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using System.IO;
+namespace HotelManangementSystemLibrary.Utilities.Extensions
+{
+    public static class IHotelCollections
+    {
+        public static void SaveData<T>(this ICollectionHotel<T> data,string file) 
+            where T : IHotelModel
+        {
+            StringBuilder bl = new StringBuilder();
+            foreach (T item in data)
+                bl.AppendLine(item.ToCSVFormat());
+
+            File.WriteAllText(file, bl.ToString());
+        }//SaveData
+    }//class
+}//namespace
