a9948aff6d960b23d19dd04f81208847bee81676
Phiwokwakhe Khathwane <phiwokwakhe299@gmail.com>
2024-03-11 10:13:16 +0200

    Added service roles for the ServicePetsonelle

---
+++ HotelManangementSystemLibrary/Model/Enums/ServiceRole.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HotelManangementSystemLibrary
+{
+    public enum ServiceRole
+    {
+        Default = 0,
+        GuestService,
+        PhoneService
+            //More to add
+    }//enum
+}//namespace
