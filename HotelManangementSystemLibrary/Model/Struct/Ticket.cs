f7b5573bbcc814d2440538c016f128af18495609
Phiwokwakhe Khathwane <phiwokwakhe299@gmail.com>
2024-03-11 10:12:53 +0200

    Added a structure for the ticket details of the IServicePersonel

---
+++ HotelManangementSystemLibrary/Model/Struct/Ticket.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HotelManangementSystemLibrary
+{
+    public struct Ticket
+    {
+        public int TicketID { get; private set; }
+        public string Description { get; private set; }
+        public Ticket(int id, string description)
+        {
+            TicketID = id;
+            Description = description;
+        }//ctor 01
+    }//clas
+}//namespace
