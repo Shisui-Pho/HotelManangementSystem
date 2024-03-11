899acd9eedab0f6f8b4840519831baed89c3df84
Phiwokwakhe Khathwane <phiwokwakhe299@gmail.com>
2024-03-11 10:07:58 +0200

    Added global suppressions for the collections extensions

---
+++ HotelManangementSystemLibrary/GlobalSuppressions.cs
@@ -0,0 +1,26 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Style", "IDE0060:Remove unused parameter", 
+    Justification = "<Pending>", 
+    Scope = "member", 
+    Target = "~M:HotelManangementSystemLibrary.Utilities.Extensions.IBookingsExtensions.LoadBookings(HotelManangementSystemLibrary.IRoomBookings,HotelManangementSystemLibrary.IGuests,HotelManangementSystemLibrary.IRooms)~HotelManangementSystemLibrary.IRoomBookings")]
+
+[assembly: SuppressMessage("Style", "IDE0060:Remove unused parameter", 
+    Justification = "<Pending>", 
+    Scope = "member", 
+    Target = "~M:HotelManangementSystemLibrary.Utilities.Extensions.IGuestsExtensions.LoadGuests(HotelManangementSystemLibrary.IGuests,HotelManangementSystemLibrary.IUsers)~HotelManangementSystemLibrary.IGuests")]
+
+[assembly: SuppressMessage("Style", "IDE0060:Remove unused parameter", 
+    Justification = "<Pending>", 
+    Scope = "member", 
+    Target = "~M:HotelManangementSystemLibrary.Utilities.Extensions.IRoomsExtensions.LoadRooms(HotelManangementSystemLibrary.IRooms)~HotelManangementSystemLibrary.IRooms")]
+
+[assembly: SuppressMessage("Style", "IDE0060:Remove unused parameter", 
+    Justification = "<Pending>",
+    Scope = "member", 
+    Target = "~M:HotelManangementSystemLibrary.Utilities.Extensions.IUsersExtensions.LoadUsers(HotelManangementSystemLibrary.IUsers)~HotelManangementSystemLibrary.IUsers")]
