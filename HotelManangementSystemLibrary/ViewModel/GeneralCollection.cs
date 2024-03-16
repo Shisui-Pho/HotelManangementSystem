5df19e0794f91440b8ea99fc3d69730b15e3e420
Phiwokwakhe Khathwane <phiwokwakhe299@gmail.com>
2024-03-16 20:38:49 +0200

    Added a generalCollection datastructure.

---
+++ HotelManangementSystemLibrary/ViewModel/GeneralCollection.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HotelManangementSystemLibrary
+{
+    internal abstract class GeneralCollection<T> : ICollectionHotel<T>
+    {
+        //private data members
+        protected List<T> _collection;
+        private bool isSorted = false;
+
+        //indexers
+        public T this[int index] 
+        {
+            get
+            {
+                if (index >= Count)
+                    throw new IndexOutOfRangeException();
+                return _collection[index];
+            }//
+        }//end getter
+
+        public int Count => _collection.Count;
+
+        public event delOnRemovedEvent ItemRemovedEvent;
+        public event delOnAddedEvent ItemAddedEvent;
+        public event delOnUpdatedEvent UpdatedEvent;
+        public GeneralCollection()
+        {
+            _collection = new List<T>();
+        }//GeneralCollection
+        public GeneralCollection(List<T> lstdata)
+            => _collection = lstdata;
+        public GeneralCollection(T[] data)
+        {
+            _collection = new List<T>();
+            _collection.AddRange(data);
+        }//end ctor
+        public void Add(T item)
+        {
+            ItemAddedEvent?.Invoke(new HotelEventArgs("", "") { IsHandled = false });
+            _collection.Add(item);
+        }//Add
+
+        public void BatchSort()
+        {
+            if (!isSorted)
+            {
+                isSorted = true;
+                _collection.Sort();
+            }//end if  
+        }//BatchSort
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            foreach (T item in _collection)
+            {
+                yield return item;
+            }//end foreach
+        }//GetEnumerator
+        public void Remove(T item)
+        {
+            _collection.Remove(item);
+            ItemRemovedEvent?.Invoke(item, new HotelEventArgs("","") { IsHandled = false });
+        }//Remove
+
+        public void Update(T old, T _new)
+        {
+            int i = _collection.IndexOf(old);
+            if (i < 0)
+                throw new ArgumentException("Item was not found.");
+            _collection[i] = _new;
+            UpdatedEvent?.Invoke(old, _new, new HotelEventArgs("", "") { IsHandled = false });
+        }//Update
+    }////class
+}//namespace
