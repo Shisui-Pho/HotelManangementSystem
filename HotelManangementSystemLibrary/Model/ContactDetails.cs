e9265d1e434fd3bad651f1fec6fa0bd52be9c5bc
Phiwokwakhe Khathwane <phiwokwakhe299@gmail.com>
2024-03-30 15:05:15 +0200

    Set the default contact details to none inorder to save and load the datat correctly  from the textfile.

--- HotelManangementSystemLibrary/Model/ContactDetails.cs
+++ HotelManangementSystemLibrary/Model/ContactDetails.cs
@@ -17,6 +17,9 @@ namespace HotelManangementSystemLibrary
             CellphoneNumber = _cell;
             EmergencyNumber = _emergency;
         }//ctor
-        public ContactDetails() { }
+        public ContactDetails() 
+        {
+            EmailAddress = CellphoneNumber = EmergencyNumber = "None";
+        }
     }//class
 }//namespace
