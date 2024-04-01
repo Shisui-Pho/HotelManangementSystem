49535b1816d49ffc1f61430e3c7e86136a58239b
Phiwokwakhe Khathwane <phiwokwakhe299@gmail.com>
2024-04-01 16:12:40 +0200

    Added a property to the transaction for the transaction date and time.

--- HotelManangementSystemLibrary/Model/TransactionArgs.cs
+++ HotelManangementSystemLibrary/Model/TransactionArgs.cs
@@ -3,14 +3,20 @@ namespace HotelManangementSystemLibrary
 {
     public class TransactionArgs : EventArgs
     {
+        private DateTime date;
         public string TypeOfTransaction { get; private set; }
         public decimal Amount { get; private set; }
+        public DateTime Date
+        {
+            get { return date; }
+        }
         public bool Cancelled { get; set; }
         public TransactionArgs(string type, decimal amount)
         {
             TypeOfTransaction = type;
             Amount = amount;
             Cancelled = false;
+            date = DateTime.Now;
         }//ctor 01
     }//class
 }//namespace
