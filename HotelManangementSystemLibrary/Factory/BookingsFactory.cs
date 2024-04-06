da25e15adf0b796377496230a38f64483a3091b2
Phiwokwakhe Khathwane <phiwokwakhe299@gmail.com>
2024-04-06 16:24:41 +0200

    Added an overloaded method to create a booking with its booking fees, this booking is the one loaded from the textfiledatabase.

--- HotelManangementSystemLibrary/Factory/BookingsFactory.cs
+++ HotelManangementSystemLibrary/Factory/BookingsFactory.cs
@@ -5,6 +5,12 @@ namespace HotelManangementSystemLibrary.Factory
     {
         public static IRoomBooking CreateBooking(IGuest guest, IRoom room, DateTime date, int numberOfDays = 1)
             => new RoomBooking(guest, room, date, numberOfDays);
+        internal static IRoomBooking CreateBookingWithFees(IGuest guest, IRoom room, DateTime date,  IBookingFees fees,int numberOfDays = 1)
+        {
+            RoomBooking booking = new RoomBooking(guest, room, date, numberOfDays);
+            booking.SetBookingFees(fees);
+            return booking;
+        }
         public static IRoomBookings CreateBookings()
             => new RoomBookings();
     }//class
