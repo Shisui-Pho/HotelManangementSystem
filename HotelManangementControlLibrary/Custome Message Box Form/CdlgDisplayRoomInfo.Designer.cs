a85511d9f65b93c61d6f76094dde4d8c8f98c645
Phiwokwakhe Khathwane <phiwokwakhe299@gmail.com>
2024-04-09 15:47:11 +0200

    Added a custom message box to display the room details to the guest.

---
+++ HotelManangementControlLibrary/Custome Message Box Form/CdlgDisplayRoomInfo.Designer.cs
@@ -0,0 +1,272 @@
+﻿namespace HotelManangementControlLibrary.Custome_Message_Box_Form
+{
+    partial class CdlgDisplayRoomInfo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblRoomNumber = new ComponentFactory.Krypton.Toolkit.KryptonLabel();
+            this.lblPrice = new ComponentFactory.Krypton.Toolkit.KryptonLabel();
+            this.lblTypeOfRoom = new ComponentFactory.Krypton.Toolkit.KryptonLabel();
+            this.kryptonLabel3 = new ComponentFactory.Krypton.Toolkit.KryptonLabel();
+            this.kryptonLabel2 = new ComponentFactory.Krypton.Toolkit.KryptonLabel();
+            this.kryptonLabel1 = new ComponentFactory.Krypton.Toolkit.KryptonLabel();
+            this.RoomDetails = new ComponentFactory.Krypton.Toolkit.KryptonLabel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.picRoom = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.kryptonLabel18 = new ComponentFactory.Krypton.Toolkit.KryptonLabel();
+            this.btnConfirm = new ComponentFactory.Krypton.Toolkit.KryptonButton();
+            this.kryptonLabel5 = new ComponentFactory.Krypton.Toolkit.KryptonLabel();
+            this.panel1.SuspendLayout();
+            this.panel4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picRoom)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.lblRoomNumber);
+            this.panel1.Controls.Add(this.lblPrice);
+            this.panel1.Controls.Add(this.lblTypeOfRoom);
+            this.panel1.Controls.Add(this.kryptonLabel3);
+            this.panel1.Controls.Add(this.kryptonLabel2);
+            this.panel1.Controls.Add(this.kryptonLabel1);
+            this.panel1.Controls.Add(this.RoomDetails);
+            this.panel1.Controls.Add(this.panel4);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(274, 450);
+            this.panel1.TabIndex = 0;
+            // 
+            // lblRoomNumber
+            // 
+            this.lblRoomNumber.Location = new System.Drawing.Point(136, 50);
+            this.lblRoomNumber.Margin = new System.Windows.Forms.Padding(4);
+            this.lblRoomNumber.Name = "lblRoomNumber";
+            this.lblRoomNumber.Size = new System.Drawing.Size(91, 26);
+            this.lblRoomNumber.StateCommon.ShortText.Color1 = System.Drawing.Color.RoyalBlue;
+            this.lblRoomNumber.StateCommon.ShortText.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRoomNumber.TabIndex = 9;
+            this.lblRoomNumber.Values.Text = "................";
+            // 
+            // lblPrice
+            // 
+            this.lblPrice.Location = new System.Drawing.Point(136, 100);
+            this.lblPrice.Margin = new System.Windows.Forms.Padding(4);
+            this.lblPrice.Name = "lblPrice";
+            this.lblPrice.Size = new System.Drawing.Size(63, 26);
+            this.lblPrice.StateCommon.ShortText.Color1 = System.Drawing.Color.RoyalBlue;
+            this.lblPrice.StateCommon.ShortText.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPrice.TabIndex = 9;
+            this.lblPrice.Values.Text = "Price : ";
+            // 
+            // lblTypeOfRoom
+            // 
+            this.lblTypeOfRoom.Location = new System.Drawing.Point(136, 148);
+            this.lblTypeOfRoom.Margin = new System.Windows.Forms.Padding(4);
+            this.lblTypeOfRoom.Name = "lblTypeOfRoom";
+            this.lblTypeOfRoom.Size = new System.Drawing.Size(105, 26);
+            this.lblTypeOfRoom.StateCommon.ShortText.Color1 = System.Drawing.Color.RoyalBlue;
+            this.lblTypeOfRoom.StateCommon.ShortText.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTypeOfRoom.TabIndex = 9;
+            this.lblTypeOfRoom.Values.Text = "type............";
+            // 
+            // kryptonLabel3
+            // 
+            this.kryptonLabel3.Location = new System.Drawing.Point(4, 148);
+            this.kryptonLabel3.Margin = new System.Windows.Forms.Padding(4);
+            this.kryptonLabel3.Name = "kryptonLabel3";
+            this.kryptonLabel3.Size = new System.Drawing.Size(110, 26);
+            this.kryptonLabel3.StateCommon.ShortText.Color1 = System.Drawing.Color.RoyalBlue;
+            this.kryptonLabel3.StateCommon.ShortText.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.kryptonLabel3.TabIndex = 9;
+            this.kryptonLabel3.Values.Text = "Room type : ";
+            // 
+            // kryptonLabel2
+            // 
+            this.kryptonLabel2.Location = new System.Drawing.Point(4, 100);
+            this.kryptonLabel2.Margin = new System.Windows.Forms.Padding(4);
+            this.kryptonLabel2.Name = "kryptonLabel2";
+            this.kryptonLabel2.Size = new System.Drawing.Size(63, 26);
+            this.kryptonLabel2.StateCommon.ShortText.Color1 = System.Drawing.Color.RoyalBlue;
+            this.kryptonLabel2.StateCommon.ShortText.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.kryptonLabel2.TabIndex = 9;
+            this.kryptonLabel2.Values.Text = "Price : ";
+            // 
+            // kryptonLabel1
+            // 
+            this.kryptonLabel1.Location = new System.Drawing.Point(4, 50);
+            this.kryptonLabel1.Margin = new System.Windows.Forms.Padding(4);
+            this.kryptonLabel1.Name = "kryptonLabel1";
+            this.kryptonLabel1.Size = new System.Drawing.Size(136, 26);
+            this.kryptonLabel1.StateCommon.ShortText.Color1 = System.Drawing.Color.RoyalBlue;
+            this.kryptonLabel1.StateCommon.ShortText.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.kryptonLabel1.TabIndex = 9;
+            this.kryptonLabel1.Values.Text = "Room number :";
+            // 
+            // RoomDetails
+            // 
+            this.RoomDetails.Location = new System.Drawing.Point(78, 4);
+            this.RoomDetails.Margin = new System.Windows.Forms.Padding(4);
+            this.RoomDetails.Name = "RoomDetails";
+            this.RoomDetails.Size = new System.Drawing.Size(121, 26);
+            this.RoomDetails.StateCommon.ShortText.Color1 = System.Drawing.Color.RoyalBlue;
+            this.RoomDetails.StateCommon.ShortText.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RoomDetails.TabIndex = 9;
+            this.RoomDetails.Values.Text = "Room Details";
+            // 
+            // panel4
+            // 
+            this.panel4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel4.Controls.Add(this.picRoom);
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel4.Location = new System.Drawing.Point(0, 198);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(270, 248);
+            this.panel4.TabIndex = 0;
+            // 
+            // picRoom
+            // 
+            this.picRoom.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.picRoom.Location = new System.Drawing.Point(0, 0);
+            this.picRoom.Name = "picRoom";
+            this.picRoom.Size = new System.Drawing.Size(266, 244);
+            this.picRoom.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picRoom.TabIndex = 0;
+            this.picRoom.TabStop = false;
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel2.Controls.Add(this.btnConfirm);
+            this.panel2.Controls.Add(this.kryptonLabel18);
+            this.panel2.Controls.Add(this.kryptonLabel5);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(274, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(376, 450);
+            this.panel2.TabIndex = 1;
+            // 
+            // kryptonLabel18
+            // 
+            this.kryptonLabel18.AutoSize = false;
+            this.kryptonLabel18.Location = new System.Drawing.Point(20, 37);
+            this.kryptonLabel18.Name = "kryptonLabel18";
+            this.kryptonLabel18.Size = new System.Drawing.Size(354, 281);
+            this.kryptonLabel18.StateCommon.ShortText.Color1 = System.Drawing.Color.RoyalBlue;
+            this.kryptonLabel18.StateCommon.ShortText.Font = new System.Drawing.Font("Times New Roman", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.kryptonLabel18.StateCommon.ShortText.TextH = ComponentFactory.Krypton.Toolkit.PaletteRelativeAlign.Center;
+            this.kryptonLabel18.TabIndex = 21;
+            this.kryptonLabel18.Values.Text = "ALL\r\nFEATURES \r\nHERE";
+            // 
+            // btnConfirm
+            // 
+            this.btnConfirm.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.btnConfirm.Location = new System.Drawing.Point(263, 395);
+            this.btnConfirm.Margin = new System.Windows.Forms.Padding(4);
+            this.btnConfirm.Name = "btnConfirm";
+            this.btnConfirm.Size = new System.Drawing.Size(107, 51);
+            this.btnConfirm.StateCommon.Back.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(12)))), ((int)(((byte)(194)))), ((int)(((byte)(126)))));
+            this.btnConfirm.StateCommon.Back.Color2 = System.Drawing.Color.FromArgb(((int)(((byte)(12)))), ((int)(((byte)(194)))), ((int)(((byte)(126)))));
+            this.btnConfirm.StateCommon.Border.Color2 = System.Drawing.Color.MediumAquamarine;
+            this.btnConfirm.StateCommon.Border.DrawBorders = ((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders)((((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Top | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Bottom) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Left) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Right)));
+            this.btnConfirm.StateCommon.Border.Rounding = 5;
+            this.btnConfirm.StateCommon.Border.Width = 1;
+            this.btnConfirm.StateCommon.Content.ShortText.Color1 = System.Drawing.Color.White;
+            this.btnConfirm.StateCommon.Content.ShortText.Color2 = System.Drawing.Color.White;
+            this.btnConfirm.StateCommon.Content.ShortText.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConfirm.StateDisabled.Back.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(12)))), ((int)(((byte)(194)))), ((int)(((byte)(126)))));
+            this.btnConfirm.StateDisabled.Back.Color2 = System.Drawing.Color.FromArgb(((int)(((byte)(12)))), ((int)(((byte)(194)))), ((int)(((byte)(126)))));
+            this.btnConfirm.StateNormal.Back.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(12)))), ((int)(((byte)(194)))), ((int)(((byte)(126)))));
+            this.btnConfirm.StateNormal.Back.Color2 = System.Drawing.Color.FromArgb(((int)(((byte)(12)))), ((int)(((byte)(194)))), ((int)(((byte)(126)))));
+            this.btnConfirm.StatePressed.Back.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(12)))), ((int)(((byte)(194)))), ((int)(((byte)(126)))));
+            this.btnConfirm.StatePressed.Back.Color2 = System.Drawing.Color.FromArgb(((int)(((byte)(12)))), ((int)(((byte)(194)))), ((int)(((byte)(126)))));
+            this.btnConfirm.StatePressed.Content.ShortText.Color1 = System.Drawing.Color.White;
+            this.btnConfirm.StatePressed.Content.ShortText.Color2 = System.Drawing.Color.White;
+            this.btnConfirm.StateTracking.Back.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(12)))), ((int)(((byte)(194)))), ((int)(((byte)(126)))));
+            this.btnConfirm.StateTracking.Back.Color2 = System.Drawing.Color.FromArgb(((int)(((byte)(12)))), ((int)(((byte)(194)))), ((int)(((byte)(126)))));
+            this.btnConfirm.StateTracking.Content.ShortText.Color1 = System.Drawing.Color.WhiteSmoke;
+            this.btnConfirm.StateTracking.Content.ShortText.Color2 = System.Drawing.Color.WhiteSmoke;
+            this.btnConfirm.TabIndex = 24;
+            this.btnConfirm.Values.Text = "&Okay";
+            this.btnConfirm.Visible = false;
+            // 
+            // kryptonLabel5
+            // 
+            this.kryptonLabel5.Location = new System.Drawing.Point(137, 4);
+            this.kryptonLabel5.Margin = new System.Windows.Forms.Padding(4);
+            this.kryptonLabel5.Name = "kryptonLabel5";
+            this.kryptonLabel5.Size = new System.Drawing.Size(127, 26);
+            this.kryptonLabel5.StateCommon.ShortText.Color1 = System.Drawing.Color.RoyalBlue;
+            this.kryptonLabel5.StateCommon.ShortText.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.kryptonLabel5.TabIndex = 9;
+            this.kryptonLabel5.Values.Text = "Room features";
+            // 
+            // CdlgDisplayRoomInfo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(650, 450);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "CdlgDisplayRoomInfo";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Room Info";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.picRoom)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.PictureBox picRoom;
+        public ComponentFactory.Krypton.Toolkit.KryptonButton btnConfirm;
+        private ComponentFactory.Krypton.Toolkit.KryptonLabel lblPrice;
+        private ComponentFactory.Krypton.Toolkit.KryptonLabel kryptonLabel2;
+        private ComponentFactory.Krypton.Toolkit.KryptonLabel kryptonLabel1;
+        private ComponentFactory.Krypton.Toolkit.KryptonLabel RoomDetails;
+        private ComponentFactory.Krypton.Toolkit.KryptonLabel lblRoomNumber;
+        private ComponentFactory.Krypton.Toolkit.KryptonLabel lblTypeOfRoom;
+        private ComponentFactory.Krypton.Toolkit.KryptonLabel kryptonLabel3;
+        private ComponentFactory.Krypton.Toolkit.KryptonLabel kryptonLabel5;
+        private ComponentFactory.Krypton.Toolkit.KryptonLabel kryptonLabel18;
+    }
+}
---
+++ HotelManangementControlLibrary/Custome Message Box Form/CdlgDisplayRoomInfo.cs
@@ -0,0 +1,17 @@
+﻿using HotelManangementSystemLibrary;
+using System.Windows.Forms;
+
+namespace HotelManangementControlLibrary.Custome_Message_Box_Form
+{
+    public partial class CdlgDisplayRoomInfo : Form
+    {
+        public CdlgDisplayRoomInfo(IRoom room)
+        {
+            InitializeComponent();
+            lblRoomNumber.Text = room.RoomNumber;
+            lblPrice.Text = room.Price.ToString("C2");
+            lblTypeOfRoom.Text = room.IsSingleRoom ? "Single room" : "Double room";
+            picRoom.Image = room.IsSingleRoom ? Properties.Resources.single : Properties.Resources._double;
+        }//ctor 01
+    }//class
+}//namespace
