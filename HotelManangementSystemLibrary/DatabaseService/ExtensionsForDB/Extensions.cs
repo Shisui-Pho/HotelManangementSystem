09dbe8b535364f10c077204904dc4a03c8356376
Phiwokwakhe Khathwane <phiwokwakhe299@gmail.com>
2024-04-12 11:17:58 +0200

    Renamed Extensions folder to ExtensionsForDB and Moved it to the DatabseService Folder

---
+++ HotelManangementSystemLibrary/DatabaseService/ExtensionsForDB/Extensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+namespace HotelManangementSystemLibrary.Utilities.Extensions
+{
+    public static class Extensions
+    {
+        public static List<T2> ToList<T1,T2>(this SortedList<T1,T2> sortedlist)
+        {
+            List<T2> lst = new List<T2>();
+            foreach (T2 item in sortedlist.Values)
+            {
+                lst.Add(item);
+            }
+            return lst;
+        }
+    }
+}
