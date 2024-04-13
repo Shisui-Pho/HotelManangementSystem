f8314d9828bea1040a32c4c9db974d9be00d6404
Phiwokwakhe Khathwane <phiwokwakhe299@gmail.com>
2024-04-13 14:00:52 +0200

    Added an event for when a certain property is changed on an object.

--- HotelManangementSystemLibrary/Model/Interfaces/IHotelModel.cs
+++ HotelManangementSystemLibrary/Model/Interfaces/IHotelModel.cs
@@ -4,8 +4,10 @@ using System.Text;
 
 namespace HotelManangementSystemLibrary
 {
+    public delegate void delOnPropertyChanged(string id, string field, string newVal);
     public interface IHotelModel<T> : IComparable, IEquatable<T>
     {
+        event delOnPropertyChanged PropertyChangedEvent;
         string ToCSVFormat();
     }//class
 }//namespace
