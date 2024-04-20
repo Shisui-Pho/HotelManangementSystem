118a72b3ff054bbf4b9b461dcf02770dcdf04938
Phiwokwakhe Khathwane <phiwokwakhe299@gmail.com>
2024-04-20 13:53:01 +0200

    Added a Class to keep track of the rooms booked dates.

---
+++ LibraryManangementSystemLib/Model/Interfaces/IRoomBookedDate.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelManangementSystemLibrary
+{
+    public interface IRoomBookedDate : IEnumerable<DateTime>
+    {
+        bool AddBookingDate(DateTime date, int duration);
+        bool ChangeBookingDate(DateTime olddate, int oldduration, DateTime newdate, int newduaration);
+        bool RemoveBookings(DateTime date, int duration);
+    }
+}
---
+++ LibraryManangementSystemLib/Model/RoomBookedDates.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+namespace HotelManangementSystemLibrary
+{
+    internal class RoomBookedDates : IRoomBookedDate
+    {
+        private readonly List<DateTime> BookedDates;
+        public RoomBookedDates()
+        {
+            BookedDates = new List<DateTime>();
+        }//ctor main
+        public bool AddBookingDate(DateTime date, int duration)
+        {
+            if (BookedDates.Contains(date))
+                throw new ArgumentException("Cannot booke room on this date");
+            int count = 0;
+            while(count <= duration)
+            {
+                DateTime nextdate = date.AddDays(count);
+                if (BookedDates.Contains(nextdate))
+                    throw new ArgumentException($"Room has been booked for {nextdate.ToShortDateString()}.");
+                BookedDates.Add(nextdate);
+                count++;
+            }//end while
+            return true;
+        }//AddBookingDate
+        public bool ChangeBookingDate(DateTime olddate, int oldduration, DateTime newdate, int newduaration)
+        {
+            if (!RemoveBookings(olddate, oldduration))
+                return false;
+
+            if (!AddBookingDate(newdate, newduaration))
+                return false;
+            return true;
+        }//ChangeBookingDate
+
+        public IEnumerator<DateTime> GetEnumerator()
+        {
+            return BookedDates.GetEnumerator();
+        }//GetEnumerator
+
+        public bool RemoveBookings(DateTime date,int duration)
+        {
+            while(duration >= 0)
+            {
+                bool isRemoved = BookedDates.Remove(date.AddDays(duration));
+                if (!isRemoved)
+                    throw new KeyNotFoundException($"The specified date range was now found : Date {date.AddDays(duration).ToShortDateString()}");
+                duration--;
+            }//end while
+            return true;
+        }//RemoveBookings
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }//class
+}//naespace
