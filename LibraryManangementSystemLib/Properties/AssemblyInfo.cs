17c33549eaed47b6209006ab6ea643b6c83c7569
Phiwokwakhe Khathwane <phiwokwakhe299@gmail.com>
2024-05-11 12:55:14 +0200

    Changed the assembly title to fit the project name.

--- LibraryManangementSystemLib/Properties/AssemblyInfo.cs
+++ LibraryManangementSystemLib/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("LibraryManangementSystemLib")]
+[assembly: AssemblyTitle("HotelManangementSystemLibrary")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("LibraryManangementSystemLib")]
+[assembly: AssemblyProduct("HotelManangementSystemLibrary")]
 [assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
